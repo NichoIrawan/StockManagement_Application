a1782912ed282dc1f1c319bdccfa2b60d6c91c01
NurIlmiM <mufidahnurilmi@gmail.com>
2025-05-03 23:41:03 +0700

    add Gudang

--- StockManagement/Model/Gudang.cs
+++ StockManagement/Model/Gudang.cs
@@ -8,5 +8,20 @@ namespace StockManagement.Model
 {
     class Gudang
     {
+        private string NamaGudang { get; set; }
+        private string Lokasi { get; set; }
+        private List<Barang> DaftarBarang { get; set; }
+
+        public Gudang()
+        {
+            DaftarBarang = new List<Barang>();
+        }
+
+        public Gudang(string namaGudang, string lokasi, List<Barang> daftarBarang)
+        {
+            this.NamaGudang = namaGudang;
+            this.Lokasi = lokasi;
+            this.DaftarBarang = daftarBarang;
+        }
     }
 }
