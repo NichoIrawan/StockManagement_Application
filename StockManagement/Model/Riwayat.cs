83c11ff755f76d7140ee6d0622b8726141159f29
sidqi <sidqiarasyid31@gmail.com>
2025-05-03 23:28:42 +0700

    fix Riwayat

--- StockManagement/Model/Riwayat.cs
+++ StockManagement/Model/Riwayat.cs
@@ -9,12 +9,21 @@ namespace StockManagement.Model
 {
     class Riwayat
     {
-        public int id_riwayat { get; set; }
         public DateTime tanggal { get; set; }
         public String jenis_transaksi { get; set; }
         public Barang barang { get; set; }
         public int jumlah_barang { get; set; }
         public Gudang lokasi_penyimpanan { get; set; }
         public User pic { get; set; }
+
+
+        public Riwayat(DateTime tanggal, string jenis_transaksi, Barang barang, int jumlah_barang, Gudang lokasi, User pic) {
+            this.tanggal = tanggal;
+            this.jenis_transaksi = jenis_transaksi;
+            this.barang = barang;
+            this.jumlah_barang = jumlah_barang;
+            this.lokasi_penyimpanan = lokasi_penyimpanan;
+            this.pic = pic;
+        }
     }
 }
