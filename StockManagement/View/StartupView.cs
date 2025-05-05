ada80b9dda1f958c9f63599d72ab5da10ec06297
Nicholas Irawan <nicholas2815@hotmail.com>
2025-05-05 23:43:13 +0700

    Fix some typo

--- StockManagement/View/StartupView.cs
+++ StockManagement/View/StartupView.cs
@@ -1,5 +1,6 @@
 ﻿using StockManagement.Controllers.UserController;
 using StockManagement.Models;
+using StockManagementLibrary;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -32,22 +33,22 @@ namespace StockManagement.View
             string input = Console.ReadLine();
             if (input == "1")
             {
-                Console.WriteLine("Masukkan Username:");
+                Console.WriteLine("Masukkan username:");
                 string username = Console.ReadLine();
-                Console.WriteLine("Masukkan Password:");
+                Console.WriteLine("Masukkan password:");
                 string password = Console.ReadLine();
 
-                User foundUser = userList.FirstOrDefault(u => u.Username.Equals(username) && u.Password.Equals(password));
-                Console.WriteLine("Username: " + foundUser.Username + "Password: " + foundUser.Password);
+                User foundUser = userList.FirstOrDefault(u => u.username.Equals(username) && u.password.Equals(password));
+                Console.WriteLine("username: " + foundUser.username + "password: " + foundUser.password);
                 if (foundUser != null)
                 {
                     cont.ChangeState(
-                        foundUser.Role == User.Roles.Staff ? 1 :
-                        foundUser.Role == User.Roles.Manager ? 2 :
+                        foundUser.role == Roles.Staff ? 1 :
+                        foundUser.role == Roles.Manager ? 2 :
                         3);
                 }
                 else {
-                    Console.WriteLine("Password atau User salah");
+                    Console.WriteLine("password atau User salah");
                     callMenu();
                 }
                 
@@ -55,18 +56,18 @@ namespace StockManagement.View
             }
             else if (input == "2")
             {
-                Console.WriteLine("Masukkan Username: ");
+                Console.WriteLine("Masukkan username: ");
                 string username = Console.ReadLine();
                 Console.WriteLine("Masukkan Name: ");
                 string name = Console.ReadLine();
-                Console.WriteLine("Masukkan Password:");
+                Console.WriteLine("Masukkan password:");
                 string password = Console.ReadLine();
-                Console.WriteLine("Masukkan Role(1: Staff, 2: Manager, 3: Admin): ");
+                Console.WriteLine("Masukkan role(1: Staff, 2: Manager, 3: Admin): ");
                 string role = Console.ReadLine();
                 User newUser = new User(username, name,
-                    role == "1" ? User.Roles.Staff :
-                    role == "2" ? User.Roles.Manager :
-                    User.Roles.Admin,
+                    role == "1" ? Roles.Staff :
+                    role == "2" ? Roles.Manager :
+                    Roles.Admin,
                     password);
                 userList.Add(newUser);
                 callMenu();
