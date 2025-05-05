86b0ea508b32eba3c77da1f3cc133bdef8753f27
Benaya Giuseppe Linggaputra Sinulingga <Benaya.gls@gmail.com>
2025-05-05 20:29:38 +0700

    Add runtime config

---
+++ StockManagementLibrary/Enum.cs
@@ -0,0 +1,6 @@
+﻿namespace StockManagementLibrary
+{
+    public enum Kategori { Makanan, Minuman, Keperluan_rumahtangga, Lain_lain }
+    public enum NotifApp { keluar, masuk, notif_habis, notif_expired };
+    public enum Bahasa { Indonesia, English};
+}
